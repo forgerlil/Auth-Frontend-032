43f92359697922c0f64b87e9f4aa14f7d47ef749
forgerlil <lilianforger@gmail.com>
2022-12-14 11:07:53 +0100

    Re adding hash router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 
@@ -17,8 +17,8 @@ root.classList.add(
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
